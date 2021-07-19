47d2657f703cb7ae8fc04e256209927cbf660aea
Jason Young <jyoung8607@gmail.com>
2021-07-19 02:10:38 -0400

    Revert "temp try STEER_STEP 3"
    This reverts commit 456da392b4e63d4448e8777a31090a5f02c7d5a2.

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -7,7 +7,7 @@ Ecu = car.CarParams.Ecu
 class CarControllerParams():
   def __init__(self):
     self.STEER_MAX = 300
-    self.STEER_STEP = 3              # how often we update the steer cmd
+    self.STEER_STEP = 2              # how often we update the steer cmd
     self.STEER_DELTA_UP = 7          # ~0.75s time to peak torque (255/50hz/0.75s)
     self.STEER_DELTA_DOWN = 17       # ~0.3s from peak torque to zero
     self.MIN_STEER_SPEED = 3.
